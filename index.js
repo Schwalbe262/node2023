c7b4aa39a4835c1fda6aaf094fb36e9cea3b90a3
Schwalbe262 <r1jae262@uos.ac.kr>
2023-04-12 20:45:05 +0900

    async main함수 적용

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const UOSLibrary = require('./modules/UOSLibrary.js')
 
-result = await UOSLibrary.getLibSeat("C")
-console.log(result)
+async function main() {
+  let result = await UOSLibrary.getLibSeat("C")
+  console.log(result)
 
-result = await UOSLibrary.getLibSeat("A")
-console.log(result)
+  result = await UOSLibrary.getLibSeat("A")
+  console.log(result)
 
-result = await UOSLibrary.getLibSeat("L")
-console.log(result)
+  result = await UOSLibrary.getLibSeat("L")
+  console.log(result)
+}
+
+main()
